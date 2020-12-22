b5ffbeb289d07ac843da6d121f0d7b7331deba81
Antor Mahmud <54511544+osman-mahmud-antor@users.noreply.github.com>
2020-12-22 12:40:20 +0600

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+	int data;
+	struct Node *next;
+};
+
+int main(){
+	struct Node *a = NULL;
+	struct Node *b = NULL;
+	struct Node *c = NULL;
+	
+	a = (struct Node*)malloc(sizeof(struct Node));
+	b = (struct Node*)malloc(sizeof(struct Node));
+	c = (struct Node*)malloc(sizeof(struct Node));
+	
+	a->data = 89;
+	b->data = 15;
+	c->data = 39;
+	
+	a->next = b;
+	b->next = c;
+	c->next = NULL;
+	
+	while(a != NULL){
+		printf("%d ->",a->data);
+		a = a->next;
+	}
+	return 0;
+}
+//""
